2e31a9c62b749608a085bcb90578e742dfffe918
Kento Okura <kento@okura.at>
2020-10-05 09:36:01 +0200

    add folds (vim)

--- xmonad.hs
+++ xmonad.hs
@@ -27,6 +27,7 @@ import           System.IO
 
 -- main
 --------------------------------------------------------------------------------
+-- {{{
 
 main = do
   xmproc <- spawnPipe "xmobar /home/kento/.xmonad/xmobar"
@@ -47,9 +48,11 @@ main = do
     , mouseBindings = myMouseBindings
     , manageHook = manageHooks
     }
+-- }}}
 
 --------------------------------------------------------------------------------
 -- looks
+-- {{{
 
 normalBorderColor' = "#282828"
 focusedBorderColor' = "#8f3f71"
@@ -66,28 +69,32 @@ myXPConfig = greenXPConfig
   , position = Bottom
   , height = 25
   }
+-- }}}
 
 --------------------------------------------------------------------------------
 -- hooks
+-- {{{
 manageHooks = namedScratchpadManageHook pads <+> composeOne
   [ ("uni" `isPrefixOf`) <$> title -?> doShift "uni" ]
+-- }}}
 
 --------------------------------------------------------------------------------
 -- layouts
-
+-- {{{
 myLayout = avoidStruts $ named "[]=" (smartBorders tiled) 
                      ||| named "TTT" (smartBorders (Mirror tiled)) 
-                     ||| named "[M]" (noBorders Full)
+                     ||| named "[ ]" (noBorders Full)
   where
     tiled = ResizableTall 1 (2/100) (1/2) []
-
+-- }}}
 
 --------------------------------------------------------------------------------
 -- topics
 -- todo: email topic
+-- {{{
 
 myTopics :: [Topic]
-myTopics = ["none"
+myTopics = [ "none"
            , "xm"
            , "uni"
            , "config"
@@ -120,7 +127,6 @@ myTopicConfig = def
     , ("uni",    spawn "alacritty -e abduco -a uni-session" )
     , ("site",   spawn "alacritty --working-directory site/src"
               >> spawn "alacritty --working-directory site/src/templates"
-              >> spawn "alacritty --working-directory site -e stack exec site watch"
               >> spawn "qutebrowser http://localhost:8000")
     , ("xm",     spawn "alacritty -e vim .xmonad/xmonad.hs"
               >> spawnShell)
@@ -144,25 +150,29 @@ promptedGoto = workspacePrompt myXPConfig goto
 
 promptedShfit :: X ()
 promptedShfit = workspacePrompt myXPConfig $ windows . W.shift
+-- }}}
 
 --------------------------------------------------------------------------------
 -- scratchpads
+-- {{{
 
 pads :: [NamedScratchpad]
-pads = [ NS "htop" "alacritty -t htop -e /bin/htop" (title =? "htop") htophook
-              , NS "pfetch" "alacritty --hold -t pfetch -e /bin/pfetch" (title =? "pfetch") pfetchhook
-              , NS "cava" "alacritty --hold -t cava -e /bin/cava" (title =? "cava") cavahook
-              , NS "watch" "alacritty --working-directory site/ --hold -t watch -e stack exec site watch" (title =? "watch") watchhook
-              , NS "term" "alacritty -t term" (title =? "term") termhook
+pads = [ NS "htop" "alacritty -t htop -e /bin/htop" (title =? "htop") htopHook
+              , NS "pfetch" "alacritty --hold -t pfetch -e /bin/pfetch" (title =? "pfetch") pfetchHook
+              , NS "cava" "alacritty --hold -t cava -e /bin/cava" (title =? "cava") cavaHook
+              , NS "watch" "alacritty --working-directory site/ --hold -t watch -e stack exec site watch" (title =? "watch") watchHook
+              , NS "term" "alacritty -t term" (title =? "term") termHook
               ]
-                where htophook = ( customFloating $ rr (1/3) (1/37) (2/3) (2/3)) 
-                      pfetchhook = ( customFloating $ rr (1/6) (1/37) (1/3) (1/3))
-                      cavahook = ( customFloating $ rr (4/6) (1/37) (1/3) (1/3))
-                      watchhook = ( customFloating $ rr (0) (1/37) (1/3) (2/3))
-                      termhook = ( customFloating $ rr (1/3) (1/37) (2/3) (2/3))
+                where htopHook = ( customFloating $ rr (1/3) (1/37) (2/3) (2/3)) 
+                      pfetchHook = ( customFloating $ rr (1/6) (1/37) (1/3) (1/3))
+                      cavaHook = ( customFloating $ rr (4/6) (1/37) (1/3) (1/3))
+                      watchHook = ( customFloating $ rr (0) (1/37) (1/3) (2/3))
+                      termHook = ( customFloating $ rr (1/3) (1/37) (2/3) (2/3))
                       rr = W.RationalRect
+--- }}}
 --------------------------------------------------------------------------------
 -- keys
+-- {{{ 
 
 modMask' = mod4Mask
 workspaceKeys = [xK_1..xK_9] ++ [xK_0]
@@ -170,11 +180,11 @@ workspaceKeys = [xK_1..xK_9] ++ [xK_0]
 keys' conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $ 
   [
   -- scratchpads and prompts
-    ((modMask .|. mod1Mask   , xK_t), namedScratchpadAction pads "htop")
+    ((modMask .|. mod1Mask   , xK_t),     namedScratchpadAction pads "htop")
   , ((modMask                , xK_space), namedScratchpadAction pads "term")
-  , ((modMask .|. mod1Mask   , xK_f), namedScratchpadAction pads "pfetch")
-  , ((modMask .|. mod1Mask   , xK_c), namedScratchpadAction pads "cava")
-  , ((modMask .|. mod1Mask   , xK_w), namedScratchpadAction pads "watch")
+  , ((modMask .|. mod1Mask   , xK_f),     namedScratchpadAction pads "pfetch")
+  , ((modMask .|. mod1Mask   , xK_c),     namedScratchpadAction pads "cava")
+  , ((modMask .|. mod1Mask   , xK_w),     namedScratchpadAction pads "watch")
 
   -- programs
   , ((modMask              , xK_Return), spawn "alacritty" )
@@ -197,13 +207,13 @@ keys' conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   , ((modMask .|. shiftMask, xK_k),      windows W.swapUp)
 
   -- resizing
-  , ((modMask              , xK_h),      sendMessage Shrink)
-  , ((modMask              , xK_l),      sendMessage Expand)
-  , ((modMask              , xK_u),      sendMessage MirrorShrink)
-  , ((modMask              , xK_i),      sendMessage MirrorExpand)
+  , ((modMask              , xK_h), sendMessage Shrink)
+  , ((modMask              , xK_l), sendMessage Expand)
+  , ((modMask              , xK_u), sendMessage MirrorShrink)
+  , ((modMask              , xK_i), sendMessage MirrorExpand)
 
   -- floating
-  , ((modMask .|. shiftMask, xK_t),      withFocused $ windows . W.sink)
+  , ((modMask .|. shiftMask, xK_t), withFocused $ windows . W.sink)
 
   , ((modMask              , xK_Up   ),  withFocused (keysMoveWindow (0  , -10)))
   , ((modMask              , xK_Down ),  withFocused (keysMoveWindow (0  , 10)))
@@ -241,3 +251,5 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList
   [ ((modMask,               button1), \w -> XMonad.Operations.focus w >> mouseMoveWindow w )
   , ((modMask .|. shiftMask, button1), \w -> XMonad.Operations.focus w >> Flex.mouseResizeWindow w )
   ]
+  -- }}}
+  -- vim:foldmethod=marker
